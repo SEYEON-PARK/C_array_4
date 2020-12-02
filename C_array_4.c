79bb03eed06701153c2a5a1480cd4c1f97442f90
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2020-12-03 00:13:14 +0900

    Update C_array_4.c
    C언어 array(배열) 연습!(4)

--- C_array_4.c
+++ C_array_4.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	char name[50];
-	printf("이름을 입력하세요. : ");
+	printf("이름을 입력하세요. : "); //이름 입력받기.
 	scanf_s("%s", name, sizeof(name));
 	printf("당신의 이름은 %s입니다.", name); //이름 출력하기.
 
